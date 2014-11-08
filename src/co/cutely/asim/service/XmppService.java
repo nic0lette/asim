2e69cb9e604bd2d5c00389d24d0fb7152855b50d
Johanna Amann <johanna@icir.org>
2014-11-07 21:35:50 -0800

    move back to latest smack alpha from snapshot and add required jxmpp libraries
    also - very basic connection setup

--- src/co/cutely/asim/service/XmppService.java
+++ src/co/cutely/asim/service/XmppService.java
@@ -4,8 +4,15 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smackx.nick.packet.Nick;
+
+import java.io.IOException;
 
 /**
  * Created by nicole on 11/4/14.
@@ -24,7 +31,7 @@ public class XmppService extends Service {
 	@Override
 	public int onStartCommand(final Intent intent, final int flags, final int startId) {
 		Log.i("XmppService", "Started with start id " + startId + ": " + intent);
-
+		connect();
 
 		// continue running until stopped
 		return START_STICKY;
@@ -35,5 +42,22 @@ public class XmppService extends Service {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
+	}
+
+	private void connect() {
+		AbstractXMPPConnection conn = new XMPPTCPConnection("0xxon.net");
+
+		Log.i("XMPPService", "trying to connect...");
+		try {
+			conn.connect();
+			conn.login("smacktest", "thisIsThePasswordForSmacktest");
+		} catch (SmackException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (XMPPException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
