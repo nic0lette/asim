447e284565d39c49924927a76c79c3a77e04d241
Nicolette Borrelli <nicole@hitori.org>
2014-11-08 08:33:12 -0800

    Don't name collide with every 'Account' out there...

--- src/co/cutely/asim/XmppAccount.java
+++ src/co/cutely/asim/XmppAccount.java
@@ -3,22 +3,22 @@ package co.cutely.asim;
 /**
  * A user's XMPP account
  */
-public class Account {
+public class XmppAccount {
 	public final String xmppId;
 	public final String password;
 	public final String host;
 	public final int port;
 	public final String resource;
 
-	public Account(String xmppId, String password) {
+	public XmppAccount(String xmppId, String password) {
 		this(xmppId, password, null, DEFAULT_PORT);
 	}
 
-	public Account(String xmppId, String password, String host, int port) {
+	public XmppAccount(String xmppId, String password, String host, int port) {
 		this(xmppId, password, host, port, "");
 	}
 
-	public Account(String xmppId, String password, String host, int port, String resource) {
+	public XmppAccount(String xmppId, String password, String host, int port, String resource) {
 		if (xmppId == null || password == null) {
 			throw new IllegalArgumentException("Account must include an id and password");
 		}
