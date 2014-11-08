eeb6ed92c6d640f1ab8c9c3c6c2083529071f706
Johanna Amann <johanna@icir.org>
2014-11-08 07:57:23 -0800

    slightly adapt the database to the fact that we will have to store the account for each message...

--- src/co/cutely/asim/Database.java
+++ src/co/cutely/asim/Database.java
@@ -55,7 +55,8 @@ public class Database  {
      * messages:
      * id               serial
      * timestamp        integer, default = now
-     * xmpp_id          integer (?), target or sender
+     * xmpp_id          text identify the xmpp account sending the message
+     * xmpp_target      the xmpp account we interact with
      * sender           bool, T if we sent the message
      * processed        bool, T if we read the message (received) or sent it successfully (sent)
      * message          text
@@ -71,16 +72,19 @@ public class Database  {
         public final int id;
         public final int ts;
         public final int xmpp_id; // we probably need to do this differently later...
+        public final int xmpp_target;
         public final boolean sender;
         public final boolean processed;
         public final String message;
         public final boolean xhtml;
         public final boolean otr;
 
-        public Message(int id, int ts, int xmpp_id, boolean sender, boolean processed, String message, boolean xhtml, boolean otr) {
+
+        public Message(int id, int ts, int xmpp_id, int xmpp_target, boolean sender, boolean processed, String message, boolean xhtml, boolean otr) {
             this.id = id;
             this.ts = ts;
             this.xmpp_id = xmpp_id;
+            this.xmpp_target = xmpp_target;
             this.sender = sender;
             this.processed = processed;
             this.message = message;
