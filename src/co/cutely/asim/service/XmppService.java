3a59b0d07da37483192b069b3ca01864147cc784
Johanna Amann <johanna@icir.org>
2014-11-22 22:26:29 -0800

    move XmppOtrEngineHost out of XmppService

--- src/co/cutely/asim/service/XmppService.java
+++ src/co/cutely/asim/service/XmppService.java
@@ -8,11 +8,9 @@ import android.os.IBinder;
 import android.util.Log;
 import co.cutely.asim.XmppAccount;
 import co.cutely.asim.messages.ChatMessage;
-import net.java.otr4j.OtrEngineHost;
 import net.java.otr4j.OtrException;
 import net.java.otr4j.OtrPolicy;
 import net.java.otr4j.OtrPolicyImpl;
-import net.java.otr4j.crypto.OtrCryptoEngineImpl;
 import net.java.otr4j.session.*;
 import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.packet.Message;
@@ -21,15 +19,12 @@ import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smackx.xhtmlim.XHTMLManager;
 
 import java.io.IOException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 /**
  * Service handling all XMPP connection logic
  */
-public class XmppService extends Service {
+public final class XmppService extends Service {
 	private static final String TAG = XmppService.class.getSimpleName();
 	private final IBinder xmppBinder = new XmppBinder();
 
@@ -171,7 +166,7 @@ public class XmppService extends Service {
 	}
 
 
-	private void sendMessage(final String xmppId, String target, final String message, final boolean skipDb, final boolean skipOtr) throws AccountNotConnectedException {
+	void sendMessage(final String xmppId, String target, final String message, final boolean skipDb, final boolean skipOtr) throws AccountNotConnectedException {
 		final XmppConnection conn = getConnection(xmppId);
 
 		target = stripResource(target);
@@ -348,7 +343,7 @@ public class XmppService extends Service {
 
 			Log.i(TAG, "Created otr session "+account.xmppId+" -> "+xmppId);
 			final SessionID sId = new SessionID(account.xmppId, xmppId, "XMPP");
-			s = new SessionImpl(sId, new XmppOtrEngineHost(new OtrPolicyImpl(OtrPolicy.OPPORTUNISTIC)));
+			s = new SessionImpl(sId, new XmppOtrEngineHost(XmppService.this, new OtrPolicyImpl(OtrPolicy.OPPORTUNISTIC)));
 			sessionMap.put(xmppId, s);
 			return s;
 		}
@@ -501,126 +496,4 @@ public class XmppService extends Service {
 		}
 	}
 
-	private final class XmppOtrEngineHost implements OtrEngineHost {
-		private final OtrPolicy policy;
-
-		private XmppOtrEngineHost(OtrPolicy policy) {
-			Log.i(TAG, "OTR engine host instantiated");
-			this.policy = policy;
-		}
-
-		@Override
-		public void injectMessage(SessionID sessionID, String msg) throws OtrException {
-			Log.w(TAG, "OTR inject message from "+ sessionID.getAccountID() + " to "+sessionID.getUserID()+": "+msg);
-			try {
-				sendMessage(sessionID.getAccountID(), sessionID.getUserID(), msg, true, true);
-			} catch (AccountNotConnectedException e) {
-				throw new OtrException(e);
-			}
-		}
-
-		@Override
-		public void unreadableMessageReceived(SessionID sessionID) throws OtrException {
-			Log.w(TAG, "OTR received unreadable message");
-		}
-
-		@Override
-		public void unencryptedMessageReceived(SessionID sessionID, String msg) throws OtrException {
-			Log.i(TAG, "OTR received unencrypted message "+msg);
-		}
-
-		@Override
-		public void showError(SessionID sessionID, String error) throws OtrException {
-			Log.e(TAG, "OTR error: "+error);
-		}
-
-		@Override
-		public void smpError(SessionID sessionID, int tlvType, boolean cheated) throws OtrException {
-			Log.e(TAG, "OTR smp Error: "+tlvType);
-		}
-
-		@Override
-		public void smpAborted(SessionID sessionID) throws OtrException {
-			Log.w(TAG, "OTR smp Aborted");
-		}
-
-		@Override
-		public void finishedSessionMessage(SessionID sessionID, String msgText) throws OtrException {
-			Log.i(TAG, "OTR finished session message: "+msgText);
-		}
-
-		@Override
-		public void requireEncryptedMessage(SessionID sessionID, String msgText) throws OtrException {
-			Log.i(TAG, "OTR require encrypted message: "+msgText);
-		}
-
-		@Override
-		public OtrPolicy getSessionPolicy(SessionID sessionID) {
-			return policy;
-		}
-
-		@Override
-		public FragmenterInstructions getFragmenterInstructions(SessionID sessionID) {
-			Log.i(TAG, "OTR getFragmenterIcstructions");
-			return null;
-		}
-
-		@Override
-		public KeyPair getLocalKeyPair(SessionID sessionID) throws OtrException {
-			KeyPairGenerator kg;
-			try {
-				kg = KeyPairGenerator.getInstance("DSA");
-			} catch (NoSuchAlgorithmException e) {
-				Log.e(TAG, "No such algorithm when trying to create key pair", e);
-				return null;
-			}
-			return kg.genKeyPair();
-		}
-
-		@Override
-		public byte[] getLocalFingerprintRaw(SessionID sessionID) {
-			try {
-				return new OtrCryptoEngineImpl().getFingerprintRaw(getLocalKeyPair(sessionID).getPublic());
-			} catch (OtrException e) {
-				Log.e(TAG, "Could not get key fingerprint", e);
-			}
-			return null;
-		}
-
-		@Override
-		public void askForSecret(SessionID sessionID, InstanceTag receiverTag, String question) {
-			Log.i(TAG, "OTR ask for secret: "+	question);
-		}
-
-		@Override
-		public void verify(SessionID sessionID, String fingerprint, boolean approved) {
-			Log.i(TAG, "OTR verify: "+fingerprint);
-		}
-
-		@Override
-		public void unverify(SessionID sessionID, String fingerprint) {
-			Log.i(TAG, "OTR unverify: "+fingerprint);
-		}
-
-		@Override
-		public String getReplyForUnreadableMessage(SessionID sessionID) {
-			Log.i(TAG, "OTR get reply for unreadable message");
-			return null;
-		}
-
-		@Override
-		public String getFallbackMessage(SessionID sessionID) {
-			return "An off-the-record private conversation has be requested which your client does not support";
-		}
-
-		@Override
-		public void messageFromAnotherInstanceReceived(SessionID sessionID) {
-			Log.i(TAG, "message from another instance received");
-		}
-
-		@Override
-		public void multipleInstancesDetected(SessionID sessionID) {
-			Log.i(TAG, "multiple instances detected");
-		}
-	}
 }
